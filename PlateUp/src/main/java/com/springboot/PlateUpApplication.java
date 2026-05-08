4b461c250ceda7fc24eda34df383fc5e51bbefd2
katukuri_jaswanth <katukurijaswanth2@gmail.com>
2026-05-08 14:23:18 +0530

    backend setup

---
+++ PlateUp/src/main/java/com/springboot/PlateUpApplication.java
@@ -0,0 +1,14 @@
+package com.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlateUpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlateUpApplication.class, args);
+		System.out.println("hello");
+	}
+
+}
