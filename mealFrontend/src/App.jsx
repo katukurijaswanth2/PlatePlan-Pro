6318dd76d01989d082f865363445a1828822bee9
katukuri_jaswanth <katukurijaswanth2@gmail.com>
2026-05-08 14:47:13 +0530

    feat: add axios, tailwind, and router-dom

--- mealFrontend/src/App.jsx
+++ mealFrontend/src/App.jsx
@@ -4,7 +4,10 @@ import './App.css'
 function App() {
   
   return (
-   <></>
+   <>
+     <h1 class="text-3xl font-bold underline">
+    Hello world!
+  </h1></>
   )
 }
 
--- mealFrontend/src/components/Categories.jsx
+++ mealFrontend/src/components/Categories.jsx
@@ -1,7 +1,7 @@
 import { useEffect } from "react"
 
 export const Categories=()=>{
-    useEffect
+    useEffect(()=>{},[])
     return(
         <>
         
